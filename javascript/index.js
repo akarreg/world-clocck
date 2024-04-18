cdf4d603e62c8804c7180d14c79253d896934648
akarreg <161454874+akarreg@users.noreply.github.com>
2024-04-18 14:41:17 +0200

    my java time and date screen ones

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,35 @@
 let city1Date = moment().tz("America/Los_angeles").format("MMMM DD yy");
-let dateElement = document.querySelector("#date");
-dateElement.innerHTML = city1Date;
+let city1Element = document.querySelector(" #cities1");
+let cityDateElement = cities1.querySelector("#date");
+
+cityDateElement.innerHTML = city1Date;
+
+let city1Time = moment().tz("America/Los_angeles").format(" h:mm:ss a ");
+
+let city1TimeElement = document.querySelector("#cities1");
+let cityTimeElement = cities1.querySelector("#time");
+cityTimeElement.innerHTML = city1Time;
+
 let city2Date = moment().tz("Australia/Sydney").format("MMMM DD yy");
-let date2Element = document.querySelector("#date");
-date2Element.innerHTML = city2Date;
+let city2Element = document.querySelector(" #cities2");
+let cityDate2Element = cities2.querySelector("#date");
+
+cityDate2Element.innerHTML = city2Date;
+
+let city2Time = moment().tz("Australia/Sydney").format(" h:mm:ss a ");
+
+let city2TimeElement = document.querySelector("#cities2");
+let cityTime2Element = cities2.querySelector("#time");
+cityTime2Element.innerHTML = city2Time;
+
+let city3Date = moment().tz("Europe/Lisbon").format("MMMM DD yy");
+let city3Element = document.querySelector(" #cities3");
+let cityDate3Element = cities3.querySelector("#date");
+
+cityDate3Element.innerHTML = city3Date;
+
+let city3Time = moment().tz("Europe/Lisbon").format(" h:mm:ss a ");
+
+let city3TimeElement = document.querySelector("#cities3");
+let cityTime3Element = cities3.querySelector("#time");
+cityTime3Element.innerHTML = city3Time;
