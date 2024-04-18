35a04e335fb264381120ef068ee88434738bd550
akarreg <161454874+akarreg@users.noreply.github.com>
2024-04-18 15:18:59 +0200

    my java

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,28 @@
-let city1Date = moment().tz("America/Los_angeles").format("MMMM DD yy");
-let city1Element = document.querySelector(" #cities1");
-let cityDateElement = cities1.querySelector("#date");
+setInterval(function () {
+  let city1Element = document.querySelector("#cities1");
+  let cityDateElement = city1Element.querySelector("#date");
+  let cityTimeElement = city1Element.querySelector("#time");
 
-cityDateElement.innerHTML = city1Date;
+  let city1Time = moment().tz("America/Los_angeles").format(" h:mm:ss a ");
+  let city1Date = moment().tz("America/Los_angeles").format("MMMM DO yyy");
+  cityDateElement.innerHTML = city1Date;
+  cityTimeElement.innerHTML = city1Time;
 
-let city1Time = moment().tz("America/Los_angeles").format(" h:mm:ss a ");
+  let city2Element = document.querySelector("#cities2");
+  let cityDate2Element = city2Element.querySelector("#date");
+  let cityTime2Element = city2Element.querySelector("#time");
 
-let city1TimeElement = document.querySelector("#cities1");
-let cityTimeElement = cities1.querySelector("#time");
-cityTimeElement.innerHTML = city1Time;
+  let city2Time = moment().tz("Austalia/Sydney").format(" h:mm:ss:SSS a ");
+  let city2Date = moment().tz("Austalia/Sydney").format("MMMM DO yyy");
+  cityDate2Element.innerHTML = city2Date;
+  cityTime2Element.innerHTML = city2Time;
 
-let city2Date = moment().tz("Australia/Sydney").format("MMMM DD yy");
-let city2Element = document.querySelector(" #cities2");
-let cityDate2Element = cities2.querySelector("#date");
+  let city3Element = document.querySelector("#cities3");
+  let cityDate3Element = city3Element.querySelector("#date");
+  let cityTime3Element = city3Element.querySelector("#time");
 
-cityDate2Element.innerHTML = city2Date;
-
-let city2Time = moment().tz("Australia/Sydney").format(" h:mm:ss a ");
-
-let city2TimeElement = document.querySelector("#cities2");
-let cityTime2Element = cities2.querySelector("#time");
-cityTime2Element.innerHTML = city2Time;
-
-let city3Date = moment().tz("Europe/Lisbon").format("MMMM DD yy");
-let city3Element = document.querySelector(" #cities3");
-let cityDate3Element = cities3.querySelector("#date");
-
-cityDate3Element.innerHTML = city3Date;
-
-let city3Time = moment().tz("Europe/Lisbon").format(" h:mm:ss a ");
-
-let city3TimeElement = document.querySelector("#cities3");
-let cityTime3Element = cities3.querySelector("#time");
-cityTime3Element.innerHTML = city3Time;
+  let city3Time = moment().tz("Europe/Lisbon").format(" h:mm:ss a ");
+  let city3Date = moment().tz("Europe/Lisbon").format("MMMM DO yyy");
+  cityDate3Element.innerHTML = city3Date;
+  cityTime3Element.innerHTML = city3Time;
+}, 10);
