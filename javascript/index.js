6014193fbe2c52d96aa9db7f88d413b021f80820
akarreg <161454874+akarreg@users.noreply.github.com>
2024-04-18 12:16:25 +0200

    start with java

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
-
-
-
+let city1Date = moment().tz("America/Los_angeles").format("MMMM DD yy");
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = city1Date;
+let city2Date = moment().tz("Australia/Sydney").format("MMMM DD yy");
+let date2Element = document.querySelector("#date");
+date2Element.innerHTML = city2Date;
