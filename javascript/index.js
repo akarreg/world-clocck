633db78cd7709e86344d734ea1f0a3ad6d70f1bb
akarreg <161454874+akarreg@users.noreply.github.com>
2024-04-19 13:32:16 +0200

    trouble with github

--- javascript/index.js
+++ javascript/index.js
@@ -3,17 +3,16 @@ setInterval(function () {
   let cityDateElement = city1Element.querySelector("#date");
   let cityTimeElement = city1Element.querySelector("#time");
 
-  let city1Time = moment().tz("America/Los_angeles").format(" h:mm:ss a ");
-  let city1Date = moment().tz("America/Los_angeles").format("MMMM DO yyy");
+  let city1Time = moment().tz("America/Los_angeles").format("h:mm:ss a ");
+  let city1Date = moment().tz("America/Los_angeles").format("MMMM DD yyy");
   cityDateElement.innerHTML = city1Date;
   cityTimeElement.innerHTML = city1Time;
-
   let city2Element = document.querySelector("#cities2");
   let cityDate2Element = city2Element.querySelector("#date");
   let cityTime2Element = city2Element.querySelector("#time");
 
-  let city2Time = moment().tz("Austalia/Sydney").format(" h:mm:ss:SSS a ");
-  let city2Date = moment().tz("Austalia/Sydney").format("MMMM DO yyy");
+  let city2Time = moment().tz("Australia/Sydney").format("h:mm:ss a ");
+  let city2Date = moment().tz("Australia/Sydney").format("MMMM DD yyy");
   cityDate2Element.innerHTML = city2Date;
   cityTime2Element.innerHTML = city2Time;
 
@@ -21,8 +20,16 @@ setInterval(function () {
   let cityDate3Element = city3Element.querySelector("#date");
   let cityTime3Element = city3Element.querySelector("#time");
 
-  let city3Time = moment().tz("Europe/Lisbon").format(" h:mm:ss a ");
-  let city3Date = moment().tz("Europe/Lisbon").format("MMMM DO yyy");
+  let city3Time = moment().tz("Europe/Lisbon").format("h:mm:ss:SSS a ");
+  let city3Date = moment().tz("Europe/Lisbon").format("MMMM DD yyy");
   cityDate3Element.innerHTML = city3Date;
   cityTime3Element.innerHTML = city3Time;
 }, 10);
+function update(event) {
+  let cityTimeZone = event.target.value;
+
+  cities1.innerHTML = cityTimeZone;
+}
+
+let citySelect = document.querySelector("#search");
+citySelect.addEventListener("change", update);
